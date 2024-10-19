3780277b0f42e29e7013a2d92b923046cee2bb87
annwilkowski <annmarywil@gmail.com>
2024-10-19 15:17:29 -0500

    the main page runs

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, request, jsonify
 app = Flask(__name__)
-import patient_therapist as pt
+#import patient_therapist as pt
 
 
 @app.route('/') #called when post request is made to "/"
@@ -10,11 +10,11 @@ def home():
 
 ######################### Doctor Integration ###################################
 @app.route('/doctor')
-def signup_page(): 
+def doctor(): 
     return render_template('doctor.html') #loads the signup.html website
 
 
 ######################### Patient Integration ###################################
 @app.route('/patient')
-def signup_page(): 
+def patient(): 
     return render_template('patient.html') #loads the signup.html website
