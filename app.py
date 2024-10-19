f65ee27a310735989dbdfff63f07ff2d4a6616e4
annwilkowski <annmarywil@gmail.com>
2024-10-19 15:01:27 -0500

    working on flask integration

--- app.py
+++ app.py
@@ -1,1 +1,20 @@
+from flask import Flask, render_template, request, jsonify
+app = Flask(__name__)
 import patient_therapist as pt
+
+
+@app.route('/') #called when post request is made to "/"
+def home(): 
+    return render_template('index.html') #loads the main index.html page
+
+
+######################### Doctor Integration ###################################
+@app.route('/doctor')
+def signup_page(): 
+    return render_template('doctor.html') #loads the signup.html website
+
+
+######################### Patient Integration ###################################
+@app.route('/patient')
+def signup_page(): 
+    return render_template('patient.html') #loads the signup.html website
