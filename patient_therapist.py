111f4cc271abaec10f891ce288e8e0d2d615d49d
zkaiser3 <zkaiser3@illinois.edu>
2024-10-19 15:22:53 -0500

    patient therapist library

--- patient_therapist.py
+++ patient_therapist.py
@@ -9,8 +9,8 @@ class Patient:
         # representing morning, afternoon, and evening.
         self.availability = availability
 
-        #Reason is a 1 dimensional array with 8 entries. In order, they represent depression, anxiety, ADHD, disordered
-        # eating, PTSD, LGBTQ+ specialty, substance abuse specialty, and gender preference. The first 5 entries represent
+        #Reason is a 1 dimensional array with 7 entries. In order, they represent depression, anxiety, ADHD, disordered
+        # eating, PTSD, LGBTQ+ specialty, and substance abuse specialty. The first 5 entries represent
         # a score of severity between 0 and 5. The next 2 are '1' if yes, '0' if no preference.
         self.reason = reason
 
@@ -44,7 +44,24 @@ class Therapist:
         #'Male' 'female' or 'other'
         self.gender = gender
 
+#Match uses the below helper functions to determine the total 'score' a therapist gets for a certain
+# patient. 
 def Match(patient, therapist):
+    score = 0
+
+    #no availability, gender doesn't match, no insurance = score of zero
+    if Match_availability(patient, therapist) == 0 or Match_gender(patient, therapist) == 0 or Match_insurance(patient, therapist) == 0:
+        return score
+    
+    #add availability to the score
+    score += Match_availability(patient, therapist)
+
+    #add specialty reason match to the score
+    score += Match_reason(patient, therapist)
+
+    #add price match to the score, multiplied by 5 to increase importance
+    score += 5 * (Match_prices(patient, therapist))
+    
 
 #Match_availability takes in the therapist's matrix of availabilities and patient's matrix
 # and returns the number of times of day they're both available
@@ -77,6 +94,31 @@ def Match_prices(patient, therapist):
     
 
 def Match_reason(patient, therapist):
+    #Loop will iterate 7 times for each entry in patient.reason. The first 5 entries are a score 0-25
+    # then the next 2 are either 0 or 1. The function returns the number of points based on specialty
+    # reason matching.
+    points = 0
+    for i in range(len(patient.reason)):
+        if patient.reason[i] == 0:
+            continue
+        
+        #If the index is 5 or 6 (LGBTQ+ or substance abuse specialty), add 5 points if the doctor specializes
+        # in it and the patient requested.
+        if i == 5 or i == 6:
+            if patient.reason[i] == 1 and doctor.specialty[i] == 1:
+                points += 5
+            continue
+
+        #For the first 5 common college student disorders, if the students ranks 15-25 on the scale and the
+        # therapist specializes, add 5 points.
+        elif patient.reason[i] >= 15 and therapist.specialty[i] == 1:
+            points += 5
+
+        #If the student has a mild case and ranks 1-15 and the doctor specializes in it, add 2 points.
+        elif patient.reason[i] > 0 and therapist.specialty[i] == 1:
+            points += 2
+
+    return points
     
 #This function takes the patient's array of gender preferences, which may be empty, and scores based on the therapist's
 # gender. Returns 1 if they're matched or if the patient has no preferences.
